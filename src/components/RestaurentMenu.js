76d68c10e38d8528ad923c23c355f09717f8433c
Vijay KV <vijaykv2228@gmail.com>
2023-03-29 12:05:58 +0530

    Redirects file

--- src/components/RestaurentMenu.js
+++ src/components/RestaurentMenu.js
@@ -12,18 +12,25 @@ const ResMenu=()=>{
     const [restaurant,setRestaurant]=useState({});
 
     async function getRestaurantInfo(){
-        const data = await fetch("`https://www.swiggy.com/dapi/menu/v4/full?lat=27.8973944&lng=78.0880129&menuId=${resId}");
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=9.957813344607038&lng=76.24363493174314&restaurantId=382363&submitAction=ENTER");
         const json= await data.json();
-  
-        setRestaurant(json.data);
+        console.log(json.data?.cards[0]?.card.card?.info?.groupedCard);
+       
+        setRestaurant(json.data?.cards[0]?.card.card?.info);
     }
     return(
         <>
+        
         <h1>Restaurent ID : {id}</h1>
-        <h1>Namaste </h1>
+        <h1>{restaurant.name}</h1>
         <img src={IMG_CDN_URL+ restaurant.cloudinaryImageId}/>
          {console.log(restaurant.cloudinaryImageId)};
+        <h2>{restaurant.city}</h2>
+        <h2>{restaurant.costForTwo}</h2>
+        <h2>{restaurant.avgRating}Stars</h2>
+
         </>
+        
     )
 }
 export  default ResMenu;
