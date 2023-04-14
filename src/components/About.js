155b96af6bec78d48135d098460beaf3529a4a0f
Vijay KV <vijaykv2228@gmail.com>
2023-04-14 13:05:27 +0530

    Check

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import Image from "../assets/images/about_us.png"
+import { Link } from "react-router-dom"
+import Image from "../assets/images/About_us.png"
 const About=()=>{
     return(
         <div className="">
