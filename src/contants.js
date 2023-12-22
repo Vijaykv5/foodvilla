695329debd587c2e34c0e0a895ddbddbc1d62379
vijay <vijaykv2228@gmail.com>
2023-12-22 21:39:37 +0530

    Cors policy fixed

--- src/contants.js
+++ src/contants.js
@@ -3,5 +3,5 @@ export const IMG_CDN_URL =
 
 
 export const RESTAURANT_API =
-"https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
--- src/utils/useRestraunt.js
+++ src/utils/useRestraunt.js
@@ -11,7 +11,7 @@ const useRestraunt = (id)=>{
 
     async function getRestaurantInfo(){
         
-        const data = await fetch(`https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId=${id}`);
+        const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId=${id}`);
         const json = await data.json();
         // console.log(json.data);
         
